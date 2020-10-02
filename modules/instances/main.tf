fc069c72584a5f7c82f5e90b19eca8fa7283d62f
Thomas Trividic <thomas.trividic@prestashop.com>
2020-10-02 17:29:52 +0200

    ok

--- modules/instances/main.tf
+++ modules/instances/main.tf
@@ -1,67 +1,73 @@
-# locals {
-#   release-url = "${google_storage_bucket.artifacts.url}/gcp-0.1.0.tar.gz"
-#   release-name = "gcp"
-#   release-version = "0.1.0"
-#   startup-script-url = "${google_storage_bucket.artifacts.url}/${google_storage_bucket_object.startup_script.name}"
-#   shutdown-script-url = "${google_storage_bucket.artifacts.url}/${google_storage_bucket_object.shutdown_script.name}"
-#   secret-key-base = "XmOqDnVm3BYT2ymDmpSUf6JEvNzUXrNhIE5z9HqyxSuLPoHfzOcLZzDaJKyH9ib1"
-#   service-name = basename(google_service_directory_service.api.name)
-#   service-namespace = basename(google_service_directory_namespace.mycompany.name)
+locals {
+  release_url = var.release_url
+  startup_script_url = module.elixir_storage.startup_script_url
+  shutdown_script_url = module.elixir_storage.shutdown_script_url
+  secret_key_base = var.elixir_secret_key_base
+  service_name = basename(module.elixir_network.service_name)
+  service_namespace = basename(module.elixir_network.service_namespace)
+  cluster_hostname = trimsuffix(module.elixir_network.dns_name,".")
 
-#   # VM tags for firewall rules
-#   tags = setunion(
-#     google_compute_firewall.allow_epmd.source_tags,
-#     google_compute_firewall.allow_http.source_tags,
-#     google_compute_firewall.allow_https.source_tags
-#   )
-# }
+  # VM tags for firewall rules
+  tags = setunion(
+    module.elixir_network.http_network_tags,
+    module.elixir_network.https_network_tags,
+    module.elixir_network.epmd_network_tags
+  )
+}
 
-# # VM instances
-# resource "google_compute_instance" "ex-instance" {
-#   count = 2 // number of instances
-#   name         = "ex-instance-${count.index}"
-#   machine_type = var.machine
-#   zone         = var.gcp_default_zone
-#   tags = local.tags
+# ------------------------------------------------------------------------------
+# CREATE VM TEMPLATE
+# ------------------------------------------------------------------------------
 
-#   boot_disk {
-#     initialize_params {
-#       image = var.image
-#       type = "pd-ssd"
-#       size = 100
-#     }
-#   }
+resource "google_compute_instance_template" "template" {
+  name_prefix  = var.elixir_application_name
+  machine_type = var.machine_type
+  tags = local.tags
+  labels = {
+    environment = terraform.workspace
+    langage = "elixir"
+  }
+  instance_description = "VM hosting the ${var.elixir_application_name} application"
+  can_ip_forward = true
 
-#   network_interface {
-#     network = google_compute_network.ex_vpc.name
-#     subnetwork = google_compute_subnetwork.cluster.name
+  scheduling {
+    preemptible = var.vm_preemptible
+    automatic_restart = !var.vm_preemptible
+  }
 
-#     access_config {
+  boot_disk {
+    initialize_params {
+      image = var.image
+      type = var.disk_type
+      size = var.ssd_size
+    }
+  }
 
-#     }
-    
-#   }
+  network_interface {
+    network = module.elixir_network.vpc_name
+    access_config {
+        network_tier = "PREMIUM"
+    }
+  }
+  
+  lifecycle {
+    create_before_destroy = true
+  }
 
-#   metadata = {
-#     release-url = local.release-url
-#     release-name = local.release-name
-#     release-version = local.release-version
-#     startup-script-url = local.startup-script-url
-#     shutdown-script-url = local.shutdown-script-url
-#     secret-key-base = local.secret-key-base
-#     service-name = local.service-name
-#     service-namespace = local.service-namespace
-#     region = var.gcp_region
-#     hash = var.hash
-#   }
+  metadata = {
+    release-url = local.release_url
+    startup-script-url = local.startup_script_url
+    shutdown-script-url = local.shutdown_script_url
+    secret-key-base = local.secret_key_base
+    service-name = local.service_name
+    service-namespace = local.service_namespace
+    cluster-hostname = local.cluster_hostname
+    region = var.gcp_region
+  }
 
-#   service_account {
-#     email = "api-v2-storage@mycompany-data-integration.iam.gserviceaccount.com"
-#     scopes = ["userinfo-email","cloud-platform"]
-#   }
-
-#   scheduling {
-#     preemptible = true
-#     automatic_restart = false
-#   }
-# }
+  service_account {
+    email = google_service_account.elixir_cluster_service_account.email
+    scopes = ["userinfo-email","cloud-platform"]
+  }
+  
+}
--- modules/instances/variables.tf
+++ modules/instances/variables.tf
@@ -10,13 +10,60 @@ variable "gcp_default_zone" {
   description = "GCP default zone"
 }
 
+
+# ---------------------------------------------------------------------------------------------------------------------
+# VM variables
+# ---------------------------------------------------------------------------------------------------------------------
+
 variable "image" {
   default     = "debian-cloud/debian-10"
   description = "kind of the image for the vm"
 }
 
-variable "machine" {
+variable "machine_type" {
   type        = string
   description = "Machine type to use for APIs"
   default = "n1-standard-1"
 }
+
+variable "disk_type" {
+  type        = string
+  description = "size of the disk"
+  default = "pd-ssd"
+}
+
+variable "disk_size" {
+  type        = number
+  description = "size of the disk"
+  default = 50
+}
+
+variable "vm_preemptible" {
+  type        = boolean
+  description = "are vm preamtible"
+  default = false
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Elixir application variables
+# ---------------------------------------------------------------------------------------------------------------------
+
+
+variable "release_url" {
+  type        = string
+  description = "release of the URL"
+  default = "n1-standard-1"
+}
+
+variable "elixir_secret_key_base" {
+  type        = string
+  description = "secret key to deploy and run elixir application"
+  default = "bOweqFstyZamEyoS8FgijJAgXbny7xod3UeV+YmEuKeAfE1M901MDuIxvDu00dYl"
+}
+
+variable "elixir_application_name" {
+  type        = string
+  description = "name of your elixir application"
+  default = "api"
+}
+
