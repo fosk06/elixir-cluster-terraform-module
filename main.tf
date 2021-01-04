500d1f2e40aa5aa37dad3419186f899c0947ee83
Thomas Trividic <thomas.trividic@prestashop.com>
2021-01-05 00:22:29 +0100

    remove compute ressources from main

--- main.tf
+++ main.tf
@@ -42,42 +42,6 @@ module "elixir_storage" {
     
 }
 
-# ---------------------------------------------------------------------------------------------------------------------
-# CREATE COMPUTE RESSOURCES
-# ---------------------------------------------------------------------------------------------------------------------
-
-module "elixir_compute" {
-  gcp_project_id = var.gcp_project_id
-  gcp_region  = var.gcp_region
-  gcp_default_zone    = var.gcp_default_zone
-  source = "./modules/compute"
-  image = var.image
-  machine_type = var.machine_type
-  session_affinity = var.session_affinity
-  disk_type = var.disk_type
-  disk_size = var.disk_size
-  vm_preemptible = var.vm_preemptible
-  release_url = var.release_url
-  secret_key_base = var.secret_key_base
-  startup_script_url = module.elixir_storage.startup_script_url
-  shutdown_script_url = module.elixir_storage.shutdown_script_url
-  service_name = basename(module.elixir_network.service_name)
-  service_namespace = basename(module.elixir_network.service_namespace)
-  cluster_hostname = trimsuffix(module.elixir_network.dns_name,".")
-  vpc_name = module.elixir_network.vpc_name
-  service_account_email = google_service_account.elixir_cluster_service_account.email
-  tags = setunion(
-    module.elixir_network.http_network_tags,
-    module.elixir_network.https_network_tags,
-    module.elixir_network.epmd_network_tags
-  )
-  default_autoscaler = var.default_autoscaler
-  default_autoscaler_target_cpu = var.default_autoscaler_target_cpu
-  autoscaler_min_replicas    = var.autoscaler_min_replicas
-  autoscaler_max_replicas    = var.autoscaler_max_replicas
-  autoscaler_cooldown_period = var.autoscaler_cooldown_period
-
-}
 
 # ---------------------------------------------------------------------------------------------------------------------
 # CREATE IAM PERMISSIONS AND SERVICE ACCOUNT
